0845fd8e8529d38fa7244b6249e6ac42db2743f2
ankitghoshthecreator <baruipara01@gmail.com>
2024-12-06 01:52:53 +0530

    toast massage for camera and try to open camera through intent

--- app/src/main/java/com/ankitghoshthecreator/facedetection/MainActivity.kt
+++ app/src/main/java/com/ankitghoshthecreator/facedetection/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.ankitghoshthecreator.facedetection
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +15,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        var buttonCamera=findViewById<Button>(R.id.btnCamera)
+
+        buttonCamera.setOnClickListener {
+
+            val intent=Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            if (intent.resolveActivity(packageManager)!=null){
+
+            }else{
+                Toast.makeText(this, "something went wrong", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
